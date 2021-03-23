61ebcb7f220c75bc41d3068ff8e041725fb41fc9
johncaley <60158126+johncaley@users.noreply.github.com>
2021-03-22 22:41:22 -0400

    Add files via upload

---
+++ SQL Queries/CREATE TABE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE User(
+    userID INT PRIMARY KEY,
+    username VARCHAR(64),
+    password VARCHAR(64),
+    email VARCHAR(256)
+);
---
+++ SQL Queries/INSERT INTO User.sql
@@ -0,0 +1,6 @@
+INSERT INTO User
+VALUES(1, "PaulSimon", "newPassword1", "PSimon@gmail.com");
+INSERT INTO User
+VALUES(2, "MaryShelly", "Chester34", "MaryShelly34@yahoo.com");
+INSERT INTO User
+VALUES(3, "NickDante", "qwerty!@#", "NicholasDante131@hotmailcom");
